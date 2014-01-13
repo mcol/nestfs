2d568a07b4f6db7530d8aaaa62b52c4f8391dd63
Marco Colombo <m.colombo@ed.ac.uk>
2014-01-13 15:01:10 +0000

    Merge summary.nestfs() and summary.iter1() into a proper S3 method.

--- R/summary.R
+++ R/summary.R
@@ -1,11 +1,29 @@
-summary.nestfs <- function(res) {
+## summarise the results across the nested folds
+summary.nestfs <- function(object, iter1=FALSE, ...) {
   iqr <- function(x) quantile(x, c(0.25, 0.75), na.rm=TRUE)
-  format.iqr <- function(x) sprintf("(%.2f, %.2f)", x[1], x[2])
-  nfolds <- length(res)
+  format.iqr <- function(x, n=2) sprintf("(%.*f, %.*f)", n, x[1], n, x[2])
+  num.folds <- length(object)
   sel <- NULL
-  for (fold in 1:nfolds) {
-    fs <- res[[fold]]$fs[, c("vars", "coef", "diffs", "iter")]
+  iter1.pvals <- NULL
+  iter1.diffs <- NULL
+  for (fold in 1:num.folds) {
+    fs <- object[[fold]]$fs[, c("vars", "coef", "diffs", "iter")]
     sel <- rbind(sel, fs[!is.na(fs$iter), ]) # exlude init model
+    fold.iter1 <- object[[fold]]$iter1
+    iter1.diffs <- cbind(iter1.diffs, fold.iter1$total.diff.llk)
+    iter1.pvals <- cbind(iter1.pvals, fold.iter1$p.value)
+  }
+  if (iter1) {
+    ## summarise all variables at the first iteration
+    med.diffs <- round(apply(iter1.diffs, 1, median), 2)
+    med.pvals <- round(apply(iter1.pvals, 1, median), 3)
+    iqr.diffs <- apply(iter1.diffs, 1, function(z) format.iqr(iqr(z)))
+    iqr.pvals <- apply(iter1.pvals, 1, function(z) format.iqr(iqr(z), 3))
+    res <- data.frame(row.names=rownames(fold.iter1),
+                      med.diff.llk=med.diffs, iqr.diff.llk=iqr.diffs,
+                      med.pvalue=med.pvals, iqr.pvalue=iqr.pvals)
+    res <- res[order(res$med.diff.llk, decreasing=TRUE), ]
+    return(res)
   }
   props <- tapply(sel$vars, sel$vars, length)
   coefs <- round(tapply(sel$coef, sel$vars, median), 3)
@@ -16,36 +34,12 @@ summary.nestfs <- function(res) {
   diffs.iqr <- tapply(sel$diff, sel$vars, function(z) format.iqr(iqr(z)))
   vars <- names(props)
   fullname <- tryCatch(comp2bio(vars), error=function(e) names(props))
-  ttt <- data.frame(vars, fullname, percent=round(props/nfolds * 100, 2),
+  res <- data.frame(vars, fullname, percent=round(props/num.folds * 100, 2),
                     coef=coefs, coefIQR=coefs.iqr,
                     rank=ranks, rankIQR=ranks.iqr,
                     diffLogLik=sprintf("%.3f", diffs), diffLogLikIQR=diffs.iqr,
                     stringsAsFactors=FALSE)
-  ttt <- ttt[order(-ttt$percent, ttt$rank), ]
-  rownames(ttt) <- NULL
-  return(ttt)
-}
-
-## summarise the first iteration information across the nested folds
-summary.iter1 <- function(res) {
-  iqr <- function(x) quantile(x, c(0.25, 0.75))
-  format.iqr <- function(x, n) sprintf("(%.*f, %.*f)", n, x[1], n, x[2])
-  stopifnot(class(res) == "nestfs")
-  iter1.pvals <- NULL
-  iter1.diffs <- NULL
-  num.folds <- length(res)
-  for (fold in 1:num.folds) {
-    fold.iter1 <- res[[fold]]$iter1
-    iter1.diffs <- cbind(iter1.diffs, fold.iter1$total.diff.llk)
-    iter1.pvals <- cbind(iter1.pvals, fold.iter1$p.value)
-  }
-  med.diffs <- round(apply(iter1.diffs, 1, median), 2)
-  med.pvals <- round(apply(iter1.pvals, 1, median), 3)
-  iqr.diffs <- apply(iter1.diffs, 1, function(z) format.iqr(iqr(z), 2))
-  iqr.pvals <- apply(iter1.pvals, 1, function(z) format.iqr(iqr(z), 3))
-  res <- data.frame(row.names=rownames(fold.iter1),
-                    med.diff.llk=med.diffs, iqr.diff.llk=iqr.diffs,
-                    med.pvalue=med.pvals, iqr.pvalue=iqr.pvals)
-  res <- res[order(res$med.diff.llk, decreasing=TRUE), ]
+  res <- res[order(-res$percent, res$rank), ]
+  rownames(res) <- NULL
   return(res)
 }
