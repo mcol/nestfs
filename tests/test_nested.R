mcol/nestfs

commit 0cae4fb02bdc0c3da1bf07d0fdbb72a87e384ea3

Return the coefficients of summary() instead of summary() from nested.glm().
Saving the whole summary() looks nice, but it also stores extra information
that we don't need and makes testing harder, such as the exact call used in
glm().

R/nestfs.R
@@ -376,7 +376,7 @@ nested.forward.selection <- function(x, y, init.model, family, folds, ...) {
     stopifnot(all.equal(model$obs, y[test.idx]))
     panel <- fs$panel
     fs$fs$coef <- NA
-    fs$fs$coef[match(panel, fs$fs$vars)] <- model$coef[panel]
+    fs$fs$coef[match(panel, fs$fs$vars)] <- model$summary[panel, "Estimate"]
     fs$fit <- model$fit
     fs$obs <- model$obs
     fs$test.idx <- test.idx
@@ -406,8 +406,8 @@ nested.forward.selection <- function(x, y, init.model, family, folds, ...) {
 #' A list of length equal to \code{length(folds)}, where each entry contains
 #' the following fields:
 #' \describe{
-#' \item{summary:}{Summary of the fitted model.}
-#' \item{coef:}{Coefficients of the fitted model.}
+#' \item{summary:}{Summary of the coefficients of the model fitted on the
+#'       training observations.}
 #' \item{fit:}{Predicted values for the withdrawn observations.}
 #' \item{obs:}{Observed values for the withdrawn observations.}
 #' \item{test.llk:}{Test log-likelihood.}
@@ -450,7 +450,7 @@ nested.glm <- function(x, y, folds, family, store.glm=FALSE) {
     regr <- glm(as.formula(model), data=x.train, family=family)
     y.pred <- predict(regr, newdata=x.test, type="response")
     loglik <- loglikelihood(family, y.test, y.pred, summary(regr)$dispersion)
-    res[[fold]] <- list(summary=summary(regr), coef=regr$coef,
+    res[[fold]] <- list(summary=coefficients(summary(regr)),
                         fit=y.pred, obs=y.test,
                         test.llk=loglik, test.idx=idx.test)
     if (store.glm) res[[fold]]$regr <- regr

commit 37d8bb2d1d04547b1cf9af07b975132c52137e75

Fix error when a categorical variable is selected.
The change in 0cae4fb broke the subsetting to extract the model coefficients
if factor variables were selected. The error was produced at this point in
nested.forward.selection():

   model$summary[panel, "Estimate"]

because for factor variables, the row names of model$summary also contain
the level label, so the original variable name listed in panel will not
match. As a solution, we now perform a partial match so that we can retrieve
the coefficient correctly if the factor had only two levels (the most common
case).

This will not work if the factor has 3 or more levels (as pmatch() returns
NA in this case): this means that instead of reporting a coefficient for
these variables, we will report NA. This is probably fine because reporting
just one of the many coefficients would be inappropriate anyway. Besides,
this additional column is an undocumented feature that is reported only for
convenience and is not used anywhere else in the code.

R/nestfs.R
@@ -388,9 +388,20 @@ nested.forward.selection <- function(x, y, init.model, family, folds, ...) {
     fs <- forward.selection(x.train, y.train, init.model, family, ...)
     model <- glm.inner(x[, fs$fs$vars], y, test.idx, family)
     stopifnot(all.equal(model$obs, y[test.idx]))
+
+    ## extract the model coefficients and report them in the forward selection
+    ## object.
+    ## note that factor variables will have names in model$summary that now
+    ## include the level labels: by using pmatch(), we attempt a partial match
+    ## for variable names. this works well if the factor had only two levels;
+    ## however, if the factor had more than two levels, names cannot be matched
+    ## even partially and their coefficient is set to NA (this is better than
+    ## reporting for the whole variable a coefficient that corresponds only to
+    ## one of the available levels).
     panel <- fs$panel
     fs$fs$coef <- NA
-    fs$fs$coef[match(panel, fs$fs$vars)] <- model$summary[panel, "Estimate"]
+    idx.coefs <- pmatch(panel, rownames(model$summary))
+    fs$fs$coef[match(panel, fs$fs$vars)] <- model$summary[idx.coefs, "Estimate"]
     fs$fit <- model$fit
     fs$obs <- model$obs
     fs$test.idx <- test.idx

tests/test_nested.R
@@ -19,6 +19,16 @@ test_that("nested forward selection",
                                          num.inner.folds=10, max.iters=3,
                                          verbose=FALSE)
   expect_equal(nest.binom, nest.binom.ok)
+
+  ## test the selection of categorical variables
+  X$sex.cat <- factor(X$sex)
+  X$bmi.cat <- factor(cut(X$bmi, c(-Inf, -1, 1, Inf)))
+  nest.cat <- nested.forward.selection(X, Y, ~ age, gaussian, folds,
+                                       choose.from=c("sex.cat", "bmi.cat"),
+                                       num.inner.folds=10, max.iters=3,
+                                       verbose=FALSE)
+  expect_equal(nest.cat[[1]]$panel, c("bmi.cat", "sex.cat"))
+  expect_equal(is.na(nest.cat[[1]]$fs$coef), c(TRUE, TRUE, FALSE))
 })
 
 context("nested.glm")