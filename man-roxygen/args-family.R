25b22375010a6a65cd7cfc5ce5871f0a6e39371a
Marco Colombo <m.colombo@ed.ac.uk>
2019-09-19 22:52:30 +0100

    Convert documentation template to markdown.

--- man-roxygen/args-family.R
+++ man-roxygen/args-family.R
@@ -1,4 +1,3 @@
-#' @param family Type of model fitted: either \code{gaussian()} for linear
-#'        regression or \code{binomial()} for logistic regression. This can be
-#'        specified also as a function name (\code{gaussian}) or as a string
-#'        (\code{"gaussian"}).
+#' @param family Type of model fitted: either `gaussian()` for linear regression
+#'        or `binomial()` for logistic regression. This can be specified also as
+#'        a function name (`gaussian`) or as a string (`"gaussian"`).
